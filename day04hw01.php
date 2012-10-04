121b22a5b67b2be4995d1036806e6034e51b80f9
ChristineCat <ccatania@verizon.net>
2012-10-04 17:36:10 -0400

    Homework Day 4 - Draft

---
+++ day04hw01.php
@@ -0,0 +1,62 @@
+<?php
+	// Day 4 Homework - Exercise 1
+	// Write a short program to ptint name, birthdate and favorite color
+
+	// Initialize values for variables - in real-life, these would probably be input by the user rather than set in the program
+	$YourName="Chris Catania";
+	$YourBirthDate="October 31, 1953";
+	$YourFavoriteColor="blue";
+	
+	// print sentences to include variables - each sentence on a separate line
+
+	print "My name is ";
+	print $YourName;
+	print <br>";
+	print "I was born on ";
+	print $YourBirthDate;
+	print <br>";
+	print "My favorite color is ";
+	print $YourFavoriteColor;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day04hw02.php
@@ -0,0 +1,74 @@
+<?php
+	// Day 4 Homework - Exercise 2
+	// Write a short program to calculate the number of minutes in a week 
+	// Use variables for the number of days in a week, the number of hours in a day, and the number of minutes in an hour
+
+	// Initialize values for variables
+	$DaysPerWeek=7;
+	$HoursPerDay=24;
+	$MinutesPerHour=60;
+
+	// Calculate the number of minutes in a week
+
+	$MinutesPerWeek=$DaysPerWeek*$HoursPerDay*$MinutesPerHour;
+	
+	// print the number of minutes in a week
+
+	print "There are ";
+	print $MinutesPerWeek;
+	print " minutes per week.";
+	print <br>;
+
+	// Calculate and print the number of minutes per week in the Bilky Way, where there are 26 hours per day
+
+	// Change the number of hours per day to 26 and re-calculate the number of minutes per week
+	$HoursPerDay=24;
+	$MinutesPerWeek=$DaysPerWeek*$HoursPerDay*$MinutesPerHour;
+
+	// print the number of minutes in a week in the Bilky Way
+	print "There are ";
+	print $MinutesPerWeek;
+	print " minutes per week in the Bilky Way.";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
